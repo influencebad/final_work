bf16b705ac27685ce1a998b0e4af5e8ad9145285
Александра Александровна <aishasyleimanova@gmail.com>
2024-08-29 21:04:08 +0300

    Финальная работа по автоматизации

--- Pages/mainpage.py
+++ Pages/mainpage.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage:
@@ -9,19 +7,68 @@ class MainPage:
         self.driver = driver
         self.driver.get('https://www.kinopoisk.ru')
 
-    def click_capcha(self, element):
-        self.driver.find_element(By.CSS_SELECTOR, element).click()
-
-    def click_pop_up(self, element):
-        self.driver.find_element(By.CSS_SELECTOR, element).click()
-
     def click_watch_movie(self, element):
-        self.driver.find_element(By.NAME, element).click()
+        """
+            эта функция ищет кнопку СМОТРЕТЬ и нажимает ее
+        """
+        self.driver.find_element(By.XPATH, element).click()
 
-    def click_watch_trailer(self, element):
-        self.driver.find_element(By.NAME, element).click()
+    def find_trailer(self, element):
+        """
+            эта функция ищет кнопку ТРЕЙЛЕР, и возвращает текст кнопки
+        """
+        return self.driver.find_element(By.XPATH, element).text
 
-    def waiting_element(self, driver, element):
-        wait = WebDriverWait(driver, 10)
-        wait.until(
-            EC.presence_of_element_located(By.XPATH, element))
+    def click_advanced_search(self, element):
+        """
+            эта функция ищет кнопку РАСШИРЕННЫЙ ПОИСК, и нажимает ее
+        """
+        self.driver.find_element(By.XPATH, element).click()
+
+    def find_advanced_search(self, element):
+        """
+            эта функция находит заголовок страницы и возвращает ее название
+        """
+        return self.driver.find_element(By.XPATH, element).text
+
+    def watch_tv(self, element):
+        """
+            эта функция находит кнопку смотреть ТВ  и нажимает ее
+        """
+        self.driver.find_element(By.XPATH, element).click()
+
+    def check_tv(self, element):
+        """
+            эта функция находит заголовок страницы и возвращает ее название
+        """
+        return self.driver.find_element(By.XPATH, element).text
+
+    def input_name_film(self, element, value):
+        """
+            эта функция находит поисковую строку и вводит в нее название искомого фильма
+        """
+        self.driver.find_element(By.XPATH, element).send_keys(value)
+
+    def click_find(self, element):
+        """
+            эта функция находит кнопку найти фильм и нажимает ее
+        """
+        self.driver.find_element(By.XPATH, element).click()
+
+    def check_finds(self, element):
+        """
+            эта функция находит название первого найденного на странице фильма и возвращвет его название
+        """
+        return self.driver.find_element(By.XPATH, element).text
+
+    def movie_tickets(self, element):
+        """
+            эта функция находит кнопку БИЛЕТЫ В КИНО  и нажимает ее
+        """
+        self.driver.find_element(By.XPATH, element).click()
+
+    def check_movie_tickets(self, element):
+        """
+            эта функция находит заголовок страницы и возвращает ее название
+        """
+        return self.driver.find_element(By.XPATH, element).text
--- Tests/test_ui.py
+++ Tests/test_ui.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 import pytest
+import allure
 from selenium import webdriver
 from Pages.mainpage import MainPage
 
@@ -8,21 +9,59 @@ from Pages.mainpage import MainPage
 def browser():
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.implicitly_wait(8)
+    driver.implicitly_wait(20)
     yield driver
     driver.quit()
 
 
-def test_to_watch_trailer(browser: WebDriver):
-
+@allure.title('Поиск наличия трейлера')
+def test_find_trailer(browser: WebDriver):
     main_page = MainPage(browser)
+    with allure.step('Нажать кнопку смотреть'):
+        main_page.click_watch_movie('//span[text() = "Смотреть"]')
+    with allure.step("Найти на странице кнопку ТРЕЙЛЕР"):
+        result = main_page.find_trailer('//button[text() = "Трейлер"]')
+    with allure.step("Проверить, что кнопка имеет название Трейлер"):
+        assert result == 'Трейлер'
 
-    main_page.click_capcha('.CheckboxCaptcha-Checkbox')
 
-    main_page.click_pop_up('.styles_closeIcon__Zvc5W')
+@allure.title('Найти расширенный поиск')
+def test_find_advanced_search(browser: WebDriver):
+    main_page = MainPage(browser)
+    with allure.step('Нажать кнопку РАСШИРЕННЫЫЙ ФИЛЬТР'):
+        main_page.click_advanced_search('//a[contains(@aria-label, "Расширенный поиск")]')
+    with allure.step('Проверить что открылась страница РАСШИРЕННЫЙ ПОИСК'):
+        result = main_page.find_advanced_search('//span[text() = "Расширенный поиск"]')
+        assert result == 'Расширенный поиск'
 
-    main_page.click_watch_movie()
 
-    main_page.click_watch_trailer('Trailer')
+@allure.title('Найти просмотр ТВ')
+def test_tv_search(browser: WebDriver):
+    main_page = MainPage(browser)
+    with allure.step('Нажать кнопку ТЕЛЕКАНАЛЫ'):
+        main_page.watch_tv('//div[contains(@class, "sidebarContainer")]//a[normalize-space(text())="Телеканалы"]')
+    with allure.step('Проверить что открылась страница СМОТРЕТЬ КАНАЛЫ'):
+        result = main_page.check_tv('//h1[text() = "Смотреть каналы"]')
+        assert result == 'Смотреть каналы'
 
-    assert main_page.waiting_element(browser, "//div[contains(@class, 'styles_bufferedRangesBar__y1jyY')]")
+
+@allure.title('Поиск фильма по названию')
+def test_find_film(browser: WebDriver):
+    main_page = MainPage(browser)
+    with allure.step('Найти поле для поиска фильмов и ввести название'):
+         main_page.input_name_film('//input[@name="kp_query"]', "Челюсти")
+    with allure.step('Нажать кнопку поиск'):
+        main_page.click_find('//button[contains(@aria-label, "Найти") and contains (@class, "styles_root")]')
+    with allure.step('Проверить , что первый найденный фильм имеет искомое название'):
+        result = main_page.check_finds('//a[text() = "Челюсти"]')
+        assert result == 'Челюсти'
+
+
+@allure.title('Проверка работы раздела билеты')
+def test_find_movie_tickets(browser: WebDriver):
+    main_page = MainPage(browser)
+    with allure.step('Нажать кнопку БИЛЕТЫ В КИНО'):
+        main_page.movie_tickets('//div[contains(@class, "styles_sticky__mDnbt")]//a[normalize-space(text())="Билеты в кино"]')
+    with allure.step('Проверить что открылась страница Билеты в кино'):
+        result = main_page.check_movie_tickets('//h1[text() = "Билеты в кино"]')
+        assert result == 'Билеты в кино'
